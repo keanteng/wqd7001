2cd97e8a1a7311f4303223402755356265abf9e4
Khor Kean Teng <u2004763@siswa.um.edu.my>
2025-01-01 10:03:12 +0800

    update - change data, retrained model, change code

--- backend/model.py
+++ backend/model.py
@@ -1,8 +1,8 @@
-import pickle
+import joblib
 import sys
 
 def load_model(file_path):
     with open(file_path, 'rb') as f:
-        model = pickle.load(f)
+        model = joblib.load(f)
     print(f"Model loaded from {file_path}", file=sys.stderr)
     return model
